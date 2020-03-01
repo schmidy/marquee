84b75f4f71bdf45195ca8a688816bd761e80a799
schmidy <michael.g.schmidt@gmail.com>
2020-03-01 14:44:26 -0700

    Intital Commit

---
+++ server.js
@@ -0,0 +1,23 @@
+var http = require('http');
+var fs = require('fs');
+
+var jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+const { window } = new JSDOM();
+const { document } = (new JSDOM('')).window;
+global.document = document;
+
+var $ = jQuery = require('jquery')(window);
+
+const PORT=8080;
+
+fs.readFile('./index.html', function (err, html) {
+
+    if (err) throw err;
+
+    http.createServer(function(request, response) {
+        response.writeHeader(200, {"Content-Type": "text/html"});
+        response.write(html);
+        response.end();
+    }).listen(PORT);
+});
